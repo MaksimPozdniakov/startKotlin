b23be0f8d4f1070180e49c50b188c35162fd2e87
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-01-28 19:11:45 +0300

    hw2

--- src/main/kotlin/homeWorks/hw2/task/domain/Person.kt
+++ src/main/kotlin/homeWorks/hw2/task/domain/Person.kt
@@ -5,6 +5,15 @@ data class Person(
     val phone: String,
     val email: String
 ) {
+
+    // Вторичный конструктор
+    constructor(name: String, contact: String, isPhone: Boolean) :
+            this(
+                name,
+                phone = if (isPhone) contact else "",
+                email = if (isPhone) "" else contact
+            )
+
     override fun toString(): String {
         return "$name, $phone, $email"
     }
