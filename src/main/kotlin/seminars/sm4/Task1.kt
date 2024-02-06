5042e86fe67100c41b4bed6b92edcf7707000de1
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-02-06 20:24:21 +0300

    sm4

--- src/main/kotlin/seminars/sm4/Task1.kt
+++ src/main/kotlin/seminars/sm4/Task1.kt
@@ -1,5 +1,39 @@
 package main.kotlin.seminars.sm4
 
+interface SimpleLogger {
+    val prefix: String
+
+    fun log(message: String) {
+        println(prefix + message)
+    }
+}
+
+class SimpleLoggerImpl(
+    override val prefix: String
+) : SimpleLogger {
+
+    init {
+        println("SimpleLoggerImpl created")
+    }
+}
+
+// TODO StackTraceLogger
+
+class StackTraceLogger  (logger: SimpleLoggerImpl):SimpleLogger by logger
+{
+    override fun log(message: String){
+        super.log(message)
+        val stackTrace = Thread.currentThread().stackTrace
+        stackTrace.forEachIndexed{index,entry -> if(index > 0)  println(entry)}
+
+    }
+}
+
+val stackTraceLogger = StackTraceLogger(
+    SimpleLoggerImpl("MyLogger: ")
+)
+
 fun main() {
-    
+    println("Enter main()")
+    //stackTraceLogger.log("Hello world")
 }
